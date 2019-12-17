1bd8f82b6a0f5083846a975e2ad94cab567bf292
Arshad Mohamed <ArshadM@99x.lk>
2019-12-17 11:59:26 +0530

    renamed project

--- src/test/java/com/norkart/testcases/Firsttest.java
+++ src/test/java/com/norkart/testcases/Firsttest.java
@@ -3,6 +3,7 @@ package com.norkart.testcases;
 import org.testng.annotations.Test;
 
 import com.norkart.base.TestBase;
+import com.norkart.pages.Complaint;
 
 public class Firsttest extends TestBase{
 	
@@ -10,7 +11,11 @@ public class Firsttest extends TestBase{
 	public void run() throws InterruptedException {
 		
 	//	login();
-		Thread.sleep(4000);
+		
+		Complaint comp=new Complaint();
+		
+		comp.createNewComplaint();
+				
 	}
 
 }
